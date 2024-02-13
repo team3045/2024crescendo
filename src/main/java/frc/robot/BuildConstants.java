f2f23b53027ba446c1fb3a3762d511b4a7cbeff2
seetharamakash2026 <seetharamakash2026@serrahs.com>
2024-02-12 18:10:29 -0800

    accel limits

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2024crescendo-wpilib2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2024-02-12 19:17:20 CST";
+  public static final long BUILD_UNIX_TIME = 1707787040811L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants(){}
+}
