62b9b1904b3d0f9bcdf124681fadeded368eabb0
seetharamakash2026 <113375210+seetharamakash2026@users.noreply.github.com>
2024-02-13 15:27:10 -0800

    add new paths

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -98,7 +98,7 @@ public class RobotContainer {
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        return autoSub.getAutoCommand("odomTwo");
+        return autoSub.getAutoCommand("Top-Top-Top");
     }
 }
 
