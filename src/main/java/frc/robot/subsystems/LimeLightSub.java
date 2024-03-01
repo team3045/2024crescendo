a4ea829a54154f0020b581b0d3406c8b3ac38e27
Justin Hollister <hollisterjustin2025@serrahs.com>
2024-02-29 20:40:53 -0800

    limelight helper stuff

--- src/main/java/frc/robot/subsystems/LimeLightSub.java
+++ src/main/java/frc/robot/subsystems/LimeLightSub.java
@@ -7,13 +7,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
 
-import edu.wpi.first.math.geometry.CoordinateSystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.networktables.NetworkTable;
@@ -24,7 +21,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.LimelightHelpers;
-import frc.robot.Constants.EstimationConstants;
 
 public class LimeLightSub extends SubsystemBase {
   //Camera name
